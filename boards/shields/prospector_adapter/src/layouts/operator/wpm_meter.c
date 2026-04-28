6bdeecebf000eb820c4735c4da677998a79808ee
Benjamin Shockley <benjaminshockley@hotmail.com>
2026-04-28 13:56:17 -0500

    Update wpm_meter.c

--- boards/shields/prospector_adapter/src/layouts/operator/wpm_meter.c
+++ boards/shields/prospector_adapter/src/layouts/operator/wpm_meter.c
@@ -47,9 +47,9 @@ static void wpm_meter_render(int active_bars) {
         }  
         if (active_bars != prev_active_bars) {
             //int min_bar = (active_bars < prev_active_bars) ? active_bars : prev_active_bars;
-            int min_bar = 1;
+            int min_bar = 0;
             int max_bar = (active_bars > prev_active_bars) ? active_bars : prev_active_bars;
-            for (int i = min_bar; i < max_bar; i++) {
+            for (int i = min_bar; i <= max_bar; i++) {
                 lv_color_t color = (i < active_bars)
                     ? active_color
                     : lv_color_hex(DISPLAY_COLOR_WPM_BAR_INACTIVE);
